212bfdf2f3e2b534c9c2a9f9026108bd607c0691
Shrestha Ghosh <sg9767@srmist.edu.in>
2026-02-17 11:12:45 +0530

    UC2

---
+++ UC2Program.java
@@ -0,0 +1,11 @@
+public class UC2Program{
+    public static void main(String[] args){
+        System.out.println("   *     "+ "   *     "+"* * *  "+"   ** ");
+        System.out.println(" *   *   "+ " *   *   "+"*    *  "+" **      ");
+        System.out.println("*     *  "+ "*     *  "+"*    *  "+"**       ");
+        System.out.println("*     *  "+ "*     *  "+"* * *  "+"  **  ");
+        System.out.println("*     *  "+ "*     *  "+"*        "+"      * ");
+        System.out.println(" *   *   "+ " *   *   "+"*        "+" *   ** ");
+        System.out.println("   *     "+ "   *     "+"*        "+"  **  ");
+    }
+}
